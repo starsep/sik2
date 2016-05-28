096bc291b81c22918df3bff9c80fdfa439858a45
Filip Czaplicki <filipczaplicki@gmail.com>
2016-05-29 00:24:39 +0200

    libssh2 example

--- Master.cpp
+++ Master.cpp
@@ -1,6 +1,8 @@
 #include "Utility.hpp"
 #include "Master.hpp"
 #include <libssh2.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 void Master::cleanup(ExitCode exitCode) { Utility::_exit(exitCode); }
 
@@ -57,12 +59,111 @@ bool Master::checkListeningSocket(epoll_event &event) {
   return false;
 }
 
+static std::string sshExec(std::string hostname, std::string command) {
+//   std::cout << "--- SSH EXEC --- \n";
+//   std::cout << "HOSTNAME: " << hostname << "\n";
+//   std::cout << "COMMAND: " << command << "\n";
+
+  const int port = 22;
+  passwd *user = getpwuid(getuid());
+  std::string homeDirectory = user->pw_dir;
+  std::string username = "fc359081";//user->pw_name;
+
+  size_t index = hostname.find("@");
+  if (index != std::string::npos) {
+    username = hostname.substr(0, index);
+    hostname = hostname.substr(index + 1, hostname.length() - (index + 1));
+  }
+
+  Socket connection;
+  connection.connectClient(hostname, port);
+
+  LIBSSH2_SESSION *session = libssh2_session_init();
+
+  if (libssh2_session_handshake(session, connection.get()) != 0) {
+    Utility::syserr("libssh2_session_handshake");
+  }
+
+  /* Connect to the ssh-agent */
+  LIBSSH2_AGENT *agent = libssh2_agent_init(session);
+  int rc;
+  libssh2_agent_publickey *identity = nullptr, *prev_identity = nullptr;
+
+  if (!agent) {
+    fprintf(stderr, "Failure initializing ssh-agent support\n");
+    rc = 1;
+  }
+  if (libssh2_agent_connect(agent)) {
+
+    fprintf(stderr, "Failure connecting to ssh-agent\n");
+    rc = 1;
+  }
+  if (libssh2_agent_list_identities(agent)) {
+
+    fprintf(stderr, "Failure requesting identities to ssh-agent\n");
+    rc = 1;
+  }
+  while (1) {
+    rc = libssh2_agent_get_identity(agent, &identity, prev_identity);
+
+    if (rc == 1)
+      break;
+    if (rc < 0) {
+      fprintf(stderr,
+              "Failure obtaining identity from ssh-agent support\n");
+      rc = 1;
+    }
+    if (libssh2_agent_userauth(agent, username.c_str(), identity)) {
+
+      fprintf(stderr, "\tAuthentication with username %s and "
+                  "public key %s failed!\n",
+              username.c_str(), identity->comment);
+    } else {
+      fprintf(stderr, "\tAuthentication with username %s and "
+                  "public key %s succeeded!\n",
+              username.c_str(), identity->comment);
+      break;
+    }
+    prev_identity = identity;
+  }
+  if (rc) {
+    fprintf(stderr, "Couldn't continue authentication\n");
+  }
+
+  LIBSSH2_CHANNEL *channel = libssh2_channel_open_session(session);
+  if (libssh2_channel_exec(channel, command.c_str()) != 0) return "ERROR";
+
+  std::cout << "POSZLO\n";
+
+  std::string buffer(1000, 0);
+  std::string log;
+  int size;
+  while ((size = libssh2_channel_read(channel, buffer.begin().base(), buffer.length())) > 0) {
+    log += std::string(buffer.begin(), buffer.begin() + size);
+    std::cout << std::string(buffer.begin(), buffer.begin() + size) << "\n";
+  }
+
+  if (size == 0)
+    std::cout << "OK Player finished.\n" + log;
+  else
+    std::cout << "ERROR Connection interrupted.\n" + log;
+
+  libssh2_channel_close(channel);
+  libssh2_channel_free(channel);
+  libssh2_session_disconnect(session, "");
+  libssh2_session_free(session);
+
+  return log;
+}
+
 void Master::run() {
   unsigned lastPort = port;
   sock.connectServer(port);
   if (lastPort != port) {
     std::cout << port << std::endl;
   }
+
+  std::cerr << sshExec("students.mimuw.edu.pl", "ls -l") << "\n";
 
   Epoll efd{};
   efd.addEvent(sock);
